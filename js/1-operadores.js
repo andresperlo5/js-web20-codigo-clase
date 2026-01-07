6d5c6f6a7ad82adfbdff67e724c0d1812236e7e7
andresperlo <andres_perlo@hotmail.com>
2026-01-07 19:15:47 -0300

    creamos carpeta base

---
+++ js/1-operadores.js
@@ -0,0 +1,103 @@
+/* Operadores */
+/* Asignacion Simple= */
+
+/* Variable
+ iniciar la variable
+ let - const
+ let
+ asignarle un nombre
+ let nombre 
+ asignarle un valor
+ let nombre =  "Andres"
+*/
+/* el valor que esta del igual a la derecha se almacena en la varible que esta  a la izquierda del signo igual. En el ejemplo el valor "Andres" se almacena en la variable nombre*/
+/* let nombre = "Andres"; */
+
+/* Asignacion con Suma o Adicion */
+/* let num1 = 10;
+let resultadoSuma = num1 + 5;
+
+console.log(resultadoSuma);
+console.log(num1); */
+/* Cuando se quiere sumar solamente 1 */
+/* 10 + 1 */
+/* 1 + 10 */
+/* console.log(num1++); */
+/* Cuando se quiere restar solamente 1 */
+/* console.log(num1--); */
+
+/* Asignacion por resta */
+/* let num1 = 10;
+let resultadoResta = num1 - 5;
+console.log(resultadoResta); */
+
+/* ASignacion por multiplicacion */
+/* let num1 = 10;
+let resultadoMulti = num1 * 5;
+console.log(resultadoMulti); */
+
+/* Asiganacion por division */
+/* let num1 = 10;
+let resultadoDiv = num1 / 5;
+console.log(resultadoDiv);
+ */
+
+/* Asignacion por comparacion */
+/* 
+= - asignarle un valor a una variable
+== - el valor es el mismo? - Debil
+=== - el valor y el tipo de dato es el mismo? -  Estricto
+> - Mayor
+< - Menor
+>= - Mayor igual
+<= Menor Igual
+*/
+/* console.log(5 == "5");
+console.log(5 === "5"); */
+
+/* Operadores logicos 
+let num1 = 10
+let num2 = 5
+&& - Y
+ejemplo (num1 === 5 && num2 === 5)
+
+|| - O 
+ejemplo (num1 === 5 || num2 === 5)
+
+! - NOT - invierte el polaridad de los booleanos
+true - false
+
+let valor =  false
+!valor
+*/
+
+/* let nombreUsuario = "";
+
+console.log(!nombreUsuario); */
+
+/* NaN - not a number */
+/* null - undefined */
+
+/* atributos */
+/* console. */
+/* location. */
+/* screen. */
+/* windows. */
+/* document. */
+
+/* metodos o una funcion */
+
+/* Siempre se invocan o ejecutan a travez de los () */
+/* location.replace()
+.sort()
+.ceil()
+.round()
+.concat()
+.charAt() */
+
+/* 
+let nombre = ""
+
+"" - false - 0 - undefinded - null -> tipos de datos FALSE
+
+*/
---
+++ js/2-metodos_strings_numbers.js
@@ -0,0 +1,38 @@
+/* Strings */
+//const nombreUsuario = "ANDRES2026";
+/* length - cuenta los caracteres tanto especiales como comunes */
+/* trim - elimina los espacios en blanco de los extremos del string no los del medio del string */
+//console.log(nombreUsuario.trim().length);
+/* andres - Andres - AnDreS - ANDRES */
+/* toLowerCase - trasforma el string a minuscula
+ toUpperCase -  trasforma el string a mayuscula*/
+//console.log(nombreUsuario.toLowerCase()); /* length - 4 */ /* JS Posiciones */
+/* 1 - 2 - 3 - 4 */
+/* H - O - L - A */
+/* 0 - 1 - 2 - 3 */
+/* charAt */
+
+/* console.log(nombreUsuario.charAt(11)); */
+
+/* substrings */
+/* 1-2-3-4-5-6 */
+/* A-N-D-R-E-S */
+/* 0-1-2-3-4-5 */
+
+/* console.log(nombreUsuario.substring(0, 6)); */
+
+/* Prompt */
+
+/* const nombre = prompt("Ingrese su nombre de usuario");
+const apellido = prompt("ingrese su apellido");
+const contrasenia = prompt("ingrese su contraseña");
+const repetirContrasenia = prompt("Repita por favor la contraseña");
+console.log(nombre); */
+alert("hola mundo");
+const edad = prompt("ingrese su edad");
+/* parseInt - parseFloat - Number */
+/* console.log(parseInt(edad));
+console.log(parseFloat(edad)); */
+console.log(Number(edad));
+
+/* Numbers */

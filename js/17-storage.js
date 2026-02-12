1a8c8b2b50f8575df11bb977450c01710c796251
andresperlo <andres_perlo@hotmail.com>
2026-02-11 21:44:33 -0300

    clase 11/02 DOM

--- js/17-storage.js
+++ js/17-storage.js
@@ -3,7 +3,7 @@ local - grabar informacion hasta que yo la borre
 sesion - grabar informacion hasta que cierro la pestaña o el navegador
 */
 
-const productos = [
+/* const productos = [
   {
     id: 1,
     titulo: "Coca-Cola",
@@ -30,7 +30,7 @@ const productos = [
   },
 
 ];
-
+ */
 const crearNuevoProduto = () => {
 
   const titulo = prompt("Ingrese el nombre del producto")
@@ -78,3 +78,211 @@ const obtenerProductoLocalSt = JSON.parse(localStorage.getItem("productos"))  */
 
 
 /* Realizar CRUD la proxima clase 11-02-26 */
+const productos = [
+  {
+    id: 1,
+    titulo: "Coca-Cola",
+    imagen: "url",
+    precio: 2000,
+    descripcion: "la mejor del mundo",
+    bloqueo: false,
+  },
+  {
+    id: 2,
+    titulo: "Pepsi",
+    imagen: "url",
+    precio: 1500,
+    descripcion: "la segunda mejor del mundo",
+    bloqueo: false,
+  },
+  {
+    id: 3,
+    titulo: "Manaos",
+    imagen: "url",
+    precio: 1000,
+    descripcion: "la mejor de argentina",
+    bloqueo: false,
+  },
+
+];
+//Base de Datos simulada con el storage
+let productosGuardadosEnMemoriaLocal = JSON.parse(localStorage.getItem("productos")) || null
+
+if (!productosGuardadosEnMemoriaLocal) {
+  localStorage.setItem("productos", JSON.stringify(productos))
+  productosGuardadosEnMemoriaLocal = JSON.parse(localStorage.getItem("productos"))
+}
+
+const crearNuevoProdutoDentroDelStorage = () => {
+
+  const titulo = prompt("Ingrese el nombre del producto")
+  const imagen = prompt("Ingrese la imagen del producto")
+  const precio = prompt("Ingrese el precio del producto")
+  const descripcion = prompt("Ingrese la descripcion del producto")
+
+
+  /* id: productos.length ? productos[productos.length - 1].id + 1 : 1, */
+  const nuevoProducto = {
+    id: productos[productos.length - 1]?.id + 1 || 1,
+    titulo,
+    imagen,
+    precio,
+    descripcion,
+    bloqueo: true,
+  }
+
+  productosGuardadosEnMemoriaLocal.push(nuevoProducto)
+  localStorage.setItem("productos", JSON.stringify(productosGuardadosEnMemoriaLocal))
+};
+
+const mostrarProductosDelStorage = () => {
+  productosGuardadosEnMemoriaLocal.forEach((producto) => console.log(producto))
+}
+
+const mostrarProductosDelStorageHabilitados = () => {
+  productosGuardadosEnMemoriaLocal.forEach((producto) => {
+    if (!producto.bloqueo) {
+      console.log(producto)
+    }
+  })
+}
+
+const mostrarProductosDelStorageDeshabilitados = () => {
+  productosGuardadosEnMemoriaLocal.forEach((producto) => {
+    if (producto.bloqueo) {
+      console.log(producto)
+    }
+  })
+}
+
+const mostrarUnProductoDelStorage = (idProducto) => {
+
+  if (!idProducto) {
+    alert("ID Incorrecto")
+    return
+  }
+
+
+
+  const productoMostrar = productosGuardadosEnMemoriaLocal.find((producto) => producto.id === idProducto)
+  console.log(productoMostrar)
+}
+
+const actualizarProductoEnElStorage = (idProducto) => {
+
+  if (!idProducto) {
+    alert("ID Incorrecto")
+    return
+  }
+
+
+
+  const obtenerProductoActualizar = productosGuardadosEnMemoriaLocal.find((producto) => producto.id === idProducto)
+
+  const obtenerIndexDelProductoActualizar = productosGuardadosEnMemoriaLocal.findIndex((producto) => producto.id === idProducto)
+
+  const infoActualizar = Number(
+    prompt(`Ingresa el numero de lo que quieres actualizar:
+    1- Nombre del Producto: ${obtenerProductoActualizar.titulo}
+    2- Imagen: ${obtenerProductoActualizar.imagen}
+    3- Precio: ${obtenerProductoActualizar.precio}
+    4- Descripcion: ${obtenerProductoActualizar.descripcion}
+    5- Cambiar la opcion de bloqueo: ${obtenerProductoActualizar.bloqueo ? "Visible" : "No Visible"}
+    `
+    )
+  )
+
+  switch (infoActualizar) {
+    case 1:
+      const nuevoNombre = prompt("Ingrese un nuevo titulo para el producto")
+      obtenerProductoActualizar.titulo = nuevoNombre
+      break
+    case 2:
+      const nuevaImagen = prompt("Ingrese una nueva url para la imagen del producto")
+      obtenerProductoActualizar.imagen = nuevaImagen
+      break
+    case 3:
+      const nuevoPrecio = prompt("Ingrese un nuevo precio para el producto")
+      obtenerProductoActualizar.precio = nuevoPrecio
+      break
+    case 4:
+      const nuevaDescripcion = prompt("Ingrese una nueva descripcion para el producto")
+      obtenerProductoActualizar.descripcion = nuevaDescripcion
+      break
+    case 5:
+      const nuevaOpcionDeBloqueo = prompt("Ingrese la opcion de:'Visible' o 'No Visible' para el producto")
+      obtenerProductoActualizar.bloqueo = nuevaOpcionDeBloqueo
+      break
+
+    default:
+      console.log("Esta opcion no esta disponible")
+      break
+  }
+
+  productosGuardadosEnMemoriaLocal[obtenerIndexDelProductoActualizar] = obtenerProductoActualizar
+
+  localStorage.setItem("productos", JSON.stringify(productosGuardadosEnMemoriaLocal))
+};
+
+
+const eliminarFisicamenteUnProductoDelStorage = (idProducto) => {
+
+  if (!idProducto) {
+    alert("ID Incorrecto")
+    return
+  }
+
+
+
+  const confirmarEliminarUnProducto = confirm("¿Estas seguro de que quieres eliminar este producto? Ten en cuenta que una vez realizada la operacion no tendras marcha atras.")
+
+
+  if (confirmarEliminarUnProducto) {
+    productosGuardadosEnMemoriaLocal.forEach((producto, i) => {
+      if (producto.id === idProducto) {
+        productosGuardadosEnMemoriaLocal.splice(i, 1)
+        localStorage.setItem("productos", JSON.stringify(productosGuardadosEnMemoriaLocal))
+      }
+    })
+  }
+}
+
+const eliminarLogicamenteUnProductoDelStorage = (idProducto) => {
+
+  if (!idProducto) {
+    alert("ID Incorrecto")
+    return
+  }
+
+
+  const confirmarBloqueoLogicoProducto = confirm("¿Estas seguro de que quieres bloquear a este producto?")
+
+  if (confirmarBloqueoLogicoProducto) {
+    productosGuardadosEnMemoriaLocal.forEach((producto, i) => {
+      if (producto.id === idProducto) {
+        producto.bloqueo = true
+        localStorage.setItem("productos", JSON.stringify(productosGuardadosEnMemoriaLocal))
+      }
+    })
+  }
+};
+
+
+const activarLogicamenteUnProductoDelStorage = (idProducto) => {
+
+  if (!idProducto) {
+    alert("ID Incorrecto")
+    return
+  }
+
+  const confirmarActivacionLogicaDelProducto = confirm("¿Estas seguro de que quieres activar a este producto?")
+
+  if (confirmarActivacionLogicaDelProducto) {
+    productosGuardadosEnMemoriaLocal.forEach((producto, i) => {
+      if (producto.id === idProducto) {
+        producto.bloqueo = false
+        localStorage.setItem("productos", JSON.stringify(productosGuardadosEnMemoriaLocal))
+      }
+    })
+  }
+};
---
+++ js/18-dom.js
@@ -0,0 +1,62 @@
+/*
+D - document
+O - object
+M - model
+
+*/
+
+/* Obtener un elemento HTML */
+/* const tituloPrincipal = document.getElementById("tituloPrincipal") */
+/* setTimeout(() => {
+  tituloPrincipal.innerText = "cualquier cosa"
+}, 2000); */
+/* console.log(tituloPrincipal) */
+
+/* const parrafoPrincipal = document.getElementsByClassName("clase-parrafo")
+const imagenPrincipal = document.getElementsByTagName("img") */
+
+/* querySelector busca por defecto por el nombre de las etiquetas
+pero si le paso el simbolo de (.) punto busca por clase y si le paso el simbolo
+(#) numeral busca por id.
+*/
+//const tituloPrincipal = document.querySelector("p")
+//const tituloPrincipal = document.querySelectorAll("p")
+
+
+(
+  async () => {
+    const divPadreDelTituloPrincipal = document.getElementById("divTituloPrincipal")
+    const productosFakeStore = await fetch('https://fakestoreapi.com/products')
+    const arrayDeProductos = await productosFakeStore.json()
+
+    divPadreDelTituloPrincipal.innerHTML = arrayDeProductos.map((producto) =>
+
+      `
+  <div class="col-12 col-md-6 col-lg-4 my-3">
+    <div class="card img-class-propio">
+      <img src="${producto.image}" class="card-img-top" alt="...">
+      <div class="card-body">
+        <h5 class="card-title text-truncate">${producto.title}</h5>
+        <p class="card-text ">$${producto.price}</p>
+        <p class="card-text text-truncate">${producto.description}</p>
+        <a href="#" class="btn btn-primary">Ver Mas</a>
+      </div>
+    </div>
+  </div>
+    
+    `
+    ).join("")
+
+
+  }
+)()
+
+
+
+/* const etiquetaNuevaH1 = document.createElement("h1")
+etiquetaNuevaH1.textContent = "Elemento creado desde JS"
+console.log(etiquetaNuevaH1) */
+
+/* document.body.appendChild(etiquetaNuevaH1) */
+
+/* divPadreDelTituloPrincipal.appendChild(etiquetaNuevaH1) */
